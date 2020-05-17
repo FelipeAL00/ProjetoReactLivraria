64babd6825a4e692d5ffd27a1ea0ac858085f844
Felipe <felipe.almeidaa14@gmail.com>
2020-05-17 19:20:52 -0300

    ajuste no btn da tela home alinhando a direita e abaixo da imagem do livro

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,29 +9,36 @@ function Home() {
         <div>
           <strong>Julio Verne - Viagem ao Centro da terra</strong>
         </div>
-        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
         <button>Ver Mais</button>
+        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
       </ComboBox>
       <ComboBox>
         <div>
           <strong>Julio Verne - Viagem ao Centro da terra</strong>
         </div>
-        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
         <button>Ver Mais</button>
+        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
       </ComboBox>
       <ComboBox>
         <div>
           <strong>Julio Verne - Viagem ao Centro da terra</strong>
         </div>
-        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
         <button>Ver Mais</button>
+        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
       </ComboBox>
       <ComboBox>
         <div>
           <strong>Julio Verne - Viagem ao Centro da terra</strong>
         </div>
-        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
         <button>Ver Mais</button>
+        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
+      </ComboBox>
+      <ComboBox>
+        <div>
+          <strong>Julio Verne - Viagem ao Centro da terra</strong>
+        </div>
+        <button>Ver Mais</button>
+        <img  src={lib} alt="Julio Verne - Viagem ao Centro da Terra"/>
       </ComboBox>
   </Container>;
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -11,13 +11,12 @@ export const Container = styled.div`
   margin-top: 15%;
   @media(min-width: 450px){
     grid-template-columns:40% 40%;
-  } 
+  }
 `;
 
 export const ComboBox = styled.div`
   width: 100%;
   height: 100%;
-  background: black;
   border-radius: 2px;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.2);
   position: relative;
@@ -29,15 +28,14 @@ export const ComboBox = styled.div`
     text-align: center;
     strong {
       font-weight: bold;
-      font-size: 8pt;
+      font-size: 12pt;
       color: #333;
-      display: flex;
     }
   }
   img {
     padding-top: 15px;
     width: 100%;
-    height: 100%;
+    height: 90%;
     background: #ccc;
   }
 
@@ -46,9 +44,11 @@ export const ComboBox = styled.div`
     background: none;
     font-weight: bold;
     padding: 3px;
-    position:relative;
+    position:absolute;
     border-radius: 5%;
     color: #fff;
+    bottom:0;
+    right: 0;
 
     &:hover {
       border: 2px solid #fff;
